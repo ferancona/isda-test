e21531052f5d878bd67fdcd6eb70409102f02af9
Fernando Ancona <f.anconac@gmail.com>
2022-03-27 02:13:53 +0100

    first commit

---
+++ park_implementation.py
@@ -0,0 +1,75 @@
+from dataclasses import dataclass
+from typing import List, Optional
+
+
+@dataclass
+class Car:
+    car_length: int
+    car_time: int
+    
+    def __repr__(self) -> str:
+        return f'Car({self.car_length}, {self.car_time})'
+
+
+@dataclass
+class Spot:
+    occupant: Optional[Car]
+    length: int
+    
+    def __repr__(self) -> str:
+        return f'Spot({self.occupant}, {self.length})'
+
+
+class Park:
+    """_summary_
+    """
+    def __init__(self, park_length: int) -> None:
+        self.park_length = park_length
+        self.spots: List[Spot] = [Spot(None, self.park_length)]
+    
+    def park_car(self, car: Car) -> bool:
+        car_parked = False
+        for index, spot in enumerate(self.spots):
+            if spot.occupant is None and spot.length >= car.car_length:
+                self.spots.insert(index, Spot(car, car.car_length))
+                # If empty Spot's length is 0, remove from list.
+                if (self.spots[index + 1].length - car.car_length) == 0:
+                    self.spots.pop(index + 1)
+                else:
+                    self.spots[index + 1].length -= car.car_length
+                car_parked = True
+                break
+        return car_parked
+    
+    def elapse_period(self) -> None:
+        last_spot = self.spots[-1]
+        if isinstance(last_spot.occupant, Car):
+            last_spot.occupant.car_time -= 1
+            if last_spot.occupant.car_time == 0:
+                last_spot.occupant = None
+        
+        for index, spot in enumerate(self.spots[:-1]):
+            if isinstance(spot.occupant, Car):
+                spot.occupant.car_time -= 1
+                if spot.occupant.car_time == 0:
+                    spot.occupant = None
+            if (spot.occupant is None 
+                    and self.spots[index + 1].occupant is None):
+                spot.length += self.spots[index + 1].length
+                self.spots.pop(index + 1)
+    
+    def report_utilisation(self) -> float:
+        if len(self.spots) == 1 and self.spots[0].occupant is None:
+            return 0
+        empty_slots = sum(
+            (spot.length for spot in self.spots if spot.occupant is None)
+        )
+        return (self.park_length - empty_slots) / self.park_length
+    
+    def __repr__(self) -> str:
+        # Returns the available slots and the ones occupied by each car.
+        rep = 'Park('
+        for spot in self.spots:
+            rep += repr(spot) + ', '
+        rep = rep.rstrip(', ') + ')'
+        return rep
---
+++ park_unit_test.py
@@ -0,0 +1,199 @@
+import pytest
+
+from park_implementation import Car, Park, Spot
+
+
+@pytest.fixture
+def length1() -> int:
+    return 2
+
+
+@pytest.fixture
+def time1() -> int:
+    return 2
+
+
+@pytest.fixture
+def car1(length1, time1) -> Car:
+    return Car(
+        car_length=length1,
+        car_time=time1,
+    )
+
+
+@pytest.fixture
+def car1_repr(length1, time1) -> str:
+    return f'Car({length1}, {time1})'
+
+
+@pytest.fixture
+def length2() -> int:
+    return 4
+
+
+@pytest.fixture
+def time2() -> int:
+    return 3
+
+
+@pytest.fixture
+def car2(length2, time2) -> Car:
+    return Car(
+        car_length=length2,
+        car_time=time2,
+    )
+
+
+@pytest.fixture
+def car2_repr(length2, time2) -> str:
+    return f'Car({length2}, {time2})'
+
+
+@pytest.fixture
+def length3() -> int:
+    return 4
+
+
+@pytest.fixture
+def time3() -> int:
+    return 5
+
+
+@pytest.fixture
+def car3(length3, time3) -> Car:
+    return Car(
+        car_length=length3,
+        car_time=time3,
+    )
+
+
+@pytest.fixture
+def car3_repr(length3, time3) -> str:
+    return f'Car({length3}, {time3})'
+
+
+@pytest.fixture
+def spot1(car1) -> Spot:
+    return Spot(car1, car1.car_length)
+
+
+@pytest.fixture
+def spot2(car2) -> Spot:
+    return Spot(car2, car2.car_length)
+
+
+@pytest.fixture
+def spot3(car3) -> Spot:
+    return Spot(car3, car3.car_length)
+
+
+@pytest.fixture
+def spot1_repr(car1_repr, length1) -> Spot:
+    return f'Spot({car1_repr}, {length1})'
+
+
+@pytest.fixture
+def spot2_repr(car2_repr, length2) -> Spot:
+    return f'Spot({car2_repr}, {length2})'
+
+
+@pytest.fixture
+def spot3_repr(car3_repr, length3) -> Spot:
+    return f'Spot({car3_repr}, {length3})'
+
+
+class TestCar:
+    def test_repr(self, car1, car1_repr):
+        assert repr(car1) == car1_repr
+
+
+class TestSpot:
+    def test_repr_with_occupant(self, spot1, spot1_repr):
+        assert repr(spot1) == spot1_repr
+    
+    def test_repr_without_occupant(self):
+        spot_length = 1
+        spot = Spot(None, spot_length)
+        expected_repr = f'Spot(None, {spot_length})'
+        assert repr(spot) == expected_repr
+
+
+class TestPark:
+    @pytest.fixture
+    def park_length(self) -> int:
+        return 10
+    
+    @pytest.fixture
+    def empty_park(self, park_length) -> Park:
+        return Park(park_length=park_length)
+    
+    @pytest.fixture
+    def full_park(self, empty_park, car1, car2, car3) -> Park:
+        park = empty_park
+        park.park_car(car1)
+        park.park_car(car2)
+        park.park_car(car3)
+        return park
+    
+    def test_park_car_empty(self, empty_park, car1):
+        res = empty_park.park_car(car1)
+        car1_in_park = len(
+            [spot for spot in empty_park.spots 
+            if spot.occupant is car1]
+        ) == 1
+        assert res is True and car1_in_park
+    
+    def test_park_car_not_enough_space(self, full_park, park_length):
+        happy_car = Car(park_length + 1, 1)
+        res = full_park.park_car(happy_car)
+        happy_car_in_park = len(
+            [spot for spot in full_park.spots 
+            if spot.occupant is happy_car]
+        ) == 1
+        assert not res and not happy_car_in_park
+    
+    def test_elapse_period_car_removed(self, full_park, car1):
+        for _ in range(car1.car_time):
+            full_park.elapse_period()
+        car1_in_park = len(
+            [spot for spot in full_park.spots 
+            if spot.occupant is car1]
+        ) == 1
+        assert not car1_in_park
+    
+    def test_elapse_period_no_cars_removed(self, full_park, car1, car2, car3):
+        car_times = [car1.car_time, car2.car_time, car3.car_time]
+        for _ in range(min(car_times) - 1):
+            full_park.elapse_period()
+        cars_in_park = len(
+            [spot for spot in full_park.spots 
+            if (spot.occupant is car1
+                or spot.occupant is car2
+                or spot.occupant is car3)]
+        ) == 3
+        assert cars_in_park
+        
+    def test_report_utilisation_empty(self, empty_park):
+        assert empty_park.report_utilisation() == 0
+    
+    def test_report_utilisation_full(self, full_park):
+        assert full_park.report_utilisation() == 1
+    
+    def test_report_utilisation_fraction(self, empty_park, car1, park_length):
+        park = empty_park
+        park.park_car(car1)
+        assert park.report_utilisation() == (car1.car_length / park_length)
+    
+    def test_repr_empty(self, empty_park, park_length):
+        assert repr(empty_park) == f'Park(Spot(None, {park_length}))'
+    
+    def test_repr_full(self, full_park, spot1_repr, spot2_repr, spot3_repr):
+        expected = f'Park({spot1_repr}, {spot2_repr}, {spot3_repr})'
+        assert repr(full_park) == expected
+    
+    def test_repr_one_car(self, empty_park, car1, spot1_repr, park_length):
+        park = empty_park
+        park.park_car(car1)
+        remaining_spot_repr = f'Spot(None, {park_length - car1.car_length})'
+        expected = f'Park({spot1_repr}, {remaining_spot_repr})'
+        assert repr(park) == expected
